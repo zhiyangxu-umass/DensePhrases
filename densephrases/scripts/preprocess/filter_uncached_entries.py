43cfad54b42ba6427505d5742e1212c386faf1af
Somil Gupta <somilgupta@umass.edu>
2021-05-03 09:51:52 -0400

    Filter uncached entries script

--- densephrases/scripts/preprocess/filter_uncached_entries.py
+++ densephrases/scripts/preprocess/filter_uncached_entries.py
@@ -12,7 +12,7 @@ def filter_uncached_entries(input_data_file, input_cache_file, out_file):
     with open(input_data_file, 'r') as f:
         for entry in json.load(f)["data"]:
             ques = entry['question']
-            ques = format_query()
+            ques = format_query(ques)
             if len(entry['answers']) == 0:
                 continue
             if ques not in input_cache:
