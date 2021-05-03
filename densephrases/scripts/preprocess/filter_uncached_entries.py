6a145bb4f369ed496f80b33f25553f3727867f67
Somil Gupta <somilgupta@umass.edu>
2021-05-03 09:51:08 -0400

    Filter uncached entries script

--- densephrases/scripts/preprocess/filter_uncached_entries.py
+++ densephrases/scripts/preprocess/filter_uncached_entries.py
@@ -12,7 +12,7 @@ def filter_uncached_entries(input_data_file, input_cache_file, out_file):
     with open(input_data_file, 'r') as f:
         for entry in json.load(f)["data"]:
             ques = entry['question']
-            ques = format_query(ques[:-1] if ques.endswith('?') else ques)
+            ques = format_query()
             if len(entry['answers']) == 0:
                 continue
             if ques not in input_cache:
@@ -24,9 +24,12 @@ def filter_uncached_entries(input_data_file, input_cache_file, out_file):
 
 
 def format_query(query):
-    if not args.truecase:
-        return query
-    return truecase.get_true_case(query) if query == query.lower() else query
+    query = query[:-1] if query.endswith('?') else query
+    if args.do_lower_case:
+        query = query.lower()
+    if args.truecase:
+        query = truecase.get_true_case(query) if query == query.lower() else query
+    return query
 
 
 if __name__ == '__main__':
@@ -38,6 +41,7 @@ if __name__ == '__main__':
                                              'dph-nqsqd-pb2_dev_wiki/dump/start-pq/1048576_flat_PQ96_8/nq_test_preprocessed_pq_cache.json'))
     parser.add_argument('--truecase', default=True, action='store_true')
     parser.add_argument("--truecase_path", default='truecase/english_with_questions.dist', type=str)
+    parser.add_argument("--do_lower_case", default=False, action='store_true')
     args = parser.parse_args()
     assert os.path.exists(args.input_data_file)
     assert os.path.exists(args.input_cache)
